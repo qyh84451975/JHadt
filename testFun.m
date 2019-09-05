269c4756d529a81cd28d591fa688657776f3c188
kaffee <kaffee@yeah.net>
2019-09-05 10:12:10 +0800

    抽根烟

--- testFun.m
+++ testFun.m
@@ -1,12 +1,27 @@
 #import <Foundation/Foundation.h>
 
+NSArray *map(NSArray *items, id(^block)(id item))
+{
+	NSMutableArray *result = [NSMutableArray array];
+	for (id item in items)
+	{
+		[result addObject: block(item)];
+	}
+	return result;
+}
+
 int main(){
 
 	NSArray *testArray = @[@4, @3, @10, @44, @6, @4, @1, @7];
-	for (NSArray *foo in testArray){
-		NSLog(@"%@", foo);
+	for ( NSString *foo in testArray){
+		//以下都不行，什么玩意
+		// int v = [foo intValue];
+		// NSLog(@"%i", v);
+		// foo = [NSString stringWithFormat: @"%d", v++];
 		//有个问题，这里怎么对foo进行操作，比如自增自减呢，你会给我答案吗？
-
+		//我不会告诉你，因为我也不会。。难受
 	}
+	NSLog(@"%@", testArray);
+
 	return 0;
 }
