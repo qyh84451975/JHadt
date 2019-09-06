8912355e74406cc26f73f6f802d7713555d8c9bc
qyh <lemonkiss1996@163.com>
2019-09-06 11:03:16 +0800

    replace替换数组元素就行了

--- testFun.m
+++ testFun.m
@@ -15,26 +15,31 @@ int main() {
 	NSArray *testArray = @[@4, @3, @10, @44, @6, @4, @1, @7];
 	NSArray *array = [NSArray arrayWithObjects:
 	                  @"Amir", @"Mishal", @"Irrum", @"Adam", nil];
+	NSMutableArray *testArray2 = [NSMutableArray arrayWithObjects:@4, @3, @10, @44, @6, @4, @1, @7, nil];
 	// for ( NSString *foo in testArray){
-	// 	//以下都不行，什么玩意
-	// 	// int v = [foo intValue];
-	// 	// NSLog(@"%i", v);
-	// 	// foo = [NSString stringWithFormat: @"%d", v++];
-	// 	//有个问题，这里怎么对foo进行操作，比如自增自减呢，你会给我答案吗？
-	// 	//我不会告诉你，因为我也不会。。难受
+	//     //以下都不行，什么玩意
+	//     // int v = [foo intValue];
+	//     // NSLog(@"%i", v);
+	//     // foo = [NSString stringWithFormat: @"%d", v++];
+	//     //有个问题，这里怎么对foo进行操作，比如自增自减呢，你会给我答案吗？
+	//     //我不会告诉你，因为我也不会。。难受
 	// }
 	// 以下是可行的，平了个方
 	///////////////////////////////
+	for (int i = 0; i < testArray2.count; i++) {
+		int tmp = [[testArray2 objectAtIndex:i] intValue];
+		[testArray2 replaceObjectAtIndex:i withObject:@(++tmp)];
+	}
 	NSArray *mappedResults = map(testArray, ^id(id item) {
-       return @(pow([item intValue],3));
-    });
-    NSLog(@"%@", mappedResults);
+		return @(pow([item intValue], 3));
+	});
+	NSLog(@"%@", mappedResults);
 	///////////////////////////////
 	for (NSString * foo in testArray) {
 		int m = [foo intValue];
 		NSLog(@"%d", ++m);
 	}
-
+	NSLog(@"%@", testArray2);
 
 	return 0;
 }
